6f9bc2c2bda6df9031ee18b9dd4222fe1fdcc27c
Devin Grinstead <devin.grinstead@grinsteaddev.com>
2025-07-15 02:33:40 -0400

    Update comment docs

--- src/MimeDb.php
+++ src/MimeDb.php
@@ -8,12 +8,38 @@ namespace DGWebLLC\MimePhpDb;
  * 
  */
 class MimeDb {
-    const DATA_FILE = __DIR__.DIRECTORY_SEPARATOR."data".DIRECTORY_SEPARATOR."data";
-    private array $_data;
-
+    const DATA_FILE = Config::DATA_DIR.DIRECTORY_SEPARATOR."data";
+    /**
+     * Summary of _byName
+     * @var array
+     */
+    private array $_byName = [];
+    /**
+     * Initializes the MimeDB data container
+     */
     public function __construct() {
         if ( file_exists(self::DATA_FILE) ) {
-            $this->_data = require(self::DATA_FILE);
+            $fdata = file_get_contents(self::DATA_FILE);
+            $rows = explode("\n", $fdata);
+
+            foreach ($rows as $row) {
+                $mime = new Mime($row);
+
+                $this->_byName[$mime->name] = $mime;
+            }
         }
+    }
+    /**
+     * A pass-thru for the array_filter method, the callback provided acts directly on the internal
+     * data collection of ['name' => Mime]
+     * 
+     * Example:
+     * 
+     * $r = $mimedb->filter(function (Mime $mime) { return $mime->name == 'text/html'; });
+     * @param callable $callback
+     * @return array
+     */
+    public function filter(callable $callback) {
+        return array_filter($this->_byName, $callback);
     }
 }
