a4ec5808e1d602a66f2fc1d72f2ca595416ed764
Devin Grinstead <devin.grinstead@grinsteaddev.com>
2025-07-16 02:12:48 -0400

    Updated Test Cases

--- test/FetchTest.php
+++ test/FetchTest.php
@@ -13,28 +13,61 @@ use DGWebLLC\MimePhpDb\Fetch\Apache;
 use PHPUnit\Framework\TestCase;
 
 final class FetchTest extends TestCase {
+    const DATA_DIR = __DIR__.DIRECTORY_SEPARATOR."data";
+    public static function tearDownAfterClass(): void {
+        // Removes temporary files for next set of unit tests
+
+        $files = glob(self::DATA_DIR.DIRECTORY_SEPARATOR."*");
+
+        foreach ($files as $file) {
+            $fname = basename($file);
+
+            if ($fname != ".gitignore")
+                unlink($file);
+        }
+    }
     public function testIana(): void {
         $obj = new Iana();
 
         $obj->fetch();
-        $obj->save(Config::DATA_DIR);
+        $obj->save(self::DATA_DIR);
+
+        $this->assertFileExists(
+            self::DATA_DIR.DIRECTORY_SEPARATOR."iana",
+            "Iana failed to create data source file"
+        );
     }
     public function testNginx(): void {
         $obj = new Nginx();
 
         $obj->fetch();
-        $obj->save(Config::DATA_DIR);
+        $obj->save(self::DATA_DIR);
+
+        $this->assertFileExists(
+            self::DATA_DIR.DIRECTORY_SEPARATOR."nginx",
+            "Nginx failed to create data source file"
+        );
     }
     public function testApache(): void {
         $obj = new Apache();
 
         $obj->fetch();
-        $obj->save(Config::DATA_DIR);
+        $obj->save(self::DATA_DIR);
+
+        $this->assertFileExists(
+            self::DATA_DIR.DIRECTORY_SEPARATOR."apache",
+            "Apache failed to create data source file"
+        );
     }
     public function testCustom(): void {
         $obj = new Custom();
 
         $obj->fetch();
-        $obj->save(Config::DATA_DIR);
+        $obj->save(self::DATA_DIR);
+
+        $this->assertFileExists(
+            self::DATA_DIR.DIRECTORY_SEPARATOR."custom",
+            "Custom failed to create data source file"
+        );
     }
 }
--- test/MimeDbTest.php
+++ test/MimeDbTest.php
@@ -5,11 +5,34 @@
 namespace DGWebLLC\MimePhpDb\Test;
 
 use DGWebLLC\MimePhpDb\MimeDb;
+use DGWebLLC\MimePhpDb\ConsoleIO;
+use DGWebLLC\MimePhpDb\Scripts\Build;
 use PHPUnit\Framework\TestCase;
 
 final class MimeDbTest extends TestCase {
+    const DATA_DIR = __DIR__.DIRECTORY_SEPARATOR."data";
+    public static function tearDownAfterClass(): void {
+        // Removes temporary files for next set of unit tests
+
+        $files = glob(self::DATA_DIR.DIRECTORY_SEPARATOR."*");
+
+        foreach ($files as $file) {
+            $fname = basename($file);
+
+            if ($fname != ".gitignore")
+                unlink($file);
+        }
+    }
+    public function testBuild(): void {
+        Build::buildDataSource(new ConsoleIO(), false, self::DATA_DIR);
+
+        $this->assertFileExists(self::DATA_DIR.DIRECTORY_SEPARATOR."data", 'Failed to create data source');
+    }
+    /**
+     * @depends testBuild
+     */
     public function testIndexAccess(): void {
-        $db = new MimeDb();
+        $db = new MimeDb(self::DATA_DIR.DIRECTORY_SEPARATOR."data");
 
         $mime = $db['application/java-archive'];
 
@@ -19,8 +42,11 @@ final class MimeDbTest extends TestCase {
             'Verifies the object retrieved is for media type application/java-archive'
         );
     }
+    /**
+     * @depends testBuild
+     */
     public function testIteration(): void {
-        $db = new MimeDb();
+        $db = new MimeDb(self::DATA_DIR.DIRECTORY_SEPARATOR."data");
         $mime = null;
 
         foreach ($db as $mimeType) {
@@ -37,16 +63,16 @@ final class MimeDbTest extends TestCase {
             'Verifies the object retrieved is for media type application/java-archive'
         );
     }
-
+    /**
+     * @depends testBuild
+     */
     public function testFilter(): void {
-        $db = new MimeDb();
+        $db = new MimeDb(self::DATA_DIR.DIRECTORY_SEPARATOR."data");
         $mime = [];
 
         $mime = $db->filter(function ($m) {
             return $m->name == 'application/java-archive';
         });
-
-        print_r($mime);
 
         $this->assertNotEmpty($mime, 'Verifies that at least one match was found');
         $this->assertEquals(
